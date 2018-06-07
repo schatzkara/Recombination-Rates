2f7f6cd950007831dbc2e41493133a76112272ab
schatzk <36019128+schatzkara@users.noreply.github.com>
2018-06-06 23:55:04 -0400

    update

--- mutation_sim.py
+++ mutation_sim.py
@@ -2,10 +2,12 @@ import random
 
 nucleotides = ['A', 'T', 'C', 'G']
 
-def sim_overlaps(n, L, mu):
+def sim_mutations(n, L, mu):
+	totals = {}
 	strains = []
 	new = []
 	nucleotides = ['A', 'T', 'C', 'G']
+
 	# creates the initial strains
 	for x in range(n):
 		s = ''
@@ -13,70 +15,41 @@ def sim_overlaps(n, L, mu):
 			s+='A'
 			# s+=random.choice(nucleotides)
 		strains.append(s)
-	# return strains
-	mutations = int(mu * L)
+	# print(strains)
+
 	# mutates the strains
+	mutations = int(mu * L)
 	for s in strains:
 		t = list(s)
 		for m in range(mutations):
-			c = random.randint(1,(L-1))
-			while(t[c] == 'A'):
-				t[c] = random.choice(nucleotides)
+			f = random.randint(1,(L-1))
+			while(t[f] == 'A'):
+				t[f] = random.choice(nucleotides)
 		t = ''.join(t)
 		new.append(t)
-	print(new)
+	# print(new)
 
-	# counts up the number of overlapping mutation sites
+	# counts up actual o and c values
 	o = 0
-	for a in range(len(new)):
-		strain1 = list(new[a])
-		for b in range((a+1),len(new)):
-			strain2 = list(new[b])
-			for d in range(len(strain1)):
-				print(strain1[d] + strain2[d])
-				if (strain1[d] != 'A' and strain2[d] != 'A'):
-					o += 1
-	return o
-
-def sim_matches(n, L, mu):
-	strains = []
-	new = []
-	nucleotides = ['A', 'T', 'C', 'G']
-	# creates the initial strains
-	for x in range(n):
-		s = ''
-		for y in range(L):
-			s+='A'
-			# s+=random.choice(nucleotides)
-		strains.append(s)
-	# return strains
-	mutations = int(mu * L)
-	# mutates the strains
-	for s in strains:
-		t = list(s)
-		for m in range(mutations):
-			c = random.randint(1,(L-1))
-			while(t[c] == 'A'):
-				t[c] = random.choice(nucleotides)
-		t = ''.join(t)
-		new.append(t)
-	print(new)
-
-	# counts up the number of overlapping and matching mutation sites
 	c = 0
 	for a in range(len(new)):
 		strain1 = list(new[a])
 		for b in range((a+1),len(new)):
 			strain2 = list(new[b])
 			for d in range(len(strain1)):
-				print(strain1[d] + strain2[d])
+				# print(strain1[d] + strain2[d])
+				# counts up the number of overlapping mutation sites
+				if (strain1[d] != 'A' and strain2[d] != 'A'):
+					o += 1
+				# counts up the number of overlapping and matching mutation sites
 				if (strain1[d] != 'A' and strain1[d] == strain2[d]):
-					# print(strain1[d])
 					c += 1
-	return c
+	totals['overlaps'] = o
+	totals['matches'] = c
+
+	return totals
 
 
 
 
-print(sim_overlaps(2,10,.50))
-print(sim_matches(2,10,.50))
+# print(sim_mutations(2,10,.50))
