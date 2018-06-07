f856dd94dc0fb1720cab6c98452073a53def5980
schatzk <36019128+schatzkara@users.noreply.github.com>
2018-06-07 12:24:33 -0400

    use long doubles for sim

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 def n_choose_k(n, k):
 	if n >= k:
@@ -41,9 +42,19 @@ def overlapping_prob(o, m, n, L):
 	strain1 = n_choose_k((L-n), (m-o))
 	combos1 = n_choose_k(L, m)
 	combos2 = n_choose_k(L, n)
+	# logo = math.log10(overlaps)
+	# logs1 = math.log10(strain1)
+	# logs2 = math.log10(strain2)
+	# logc1 = math.log10(combos1)
+	# logc2 = math.log10(combos2)
+	# print('overlaps: ' + str(overlaps) + 'strain2: ' + str(strain2) + 'strain1: ' + str(strain1) + 'combos1: ' + str(combos1) + 'combos2: ' + str(combos2))
 	if(type(overlaps) is not str and type(strain1) is not str and type(strain2) is not str and type(combos1) is not str and type(combos2) is not str):
-		prob = 1/(combos1 * combos2)
-		return (overlaps * strain2 * strain1 * prob)
+		prob = np.longdouble(1/(combos1 * combos2))
+		# print('prob: ' + str(prob))
+		# print('answer: ' + str(prob * overlaps * strain2 * strain1))
+		answer = np.longdouble(prob * overlaps * strain2 * strain1)
+		return answer
+		# return (logo + logs1 + logs2 + logc1 + logc2)
 	else: 
 		return 'undefined'
 
@@ -57,34 +68,47 @@ def matching_prob(c, m, n, L, x):
 	strain1 = n_change_k((L-n), (m-c), x)
 	combos1 = n_change_k(L, m, x)
 	combos2 = n_change_k(L, n, x)
+	# logm = math.log10(matches)
+	# logs1 = math.log10(strain1)
+	# logs2 = math.log10(strain2)
+	# logc1 = math.log10(combos1)
+	# logc2 = math.log10(combos2)
 	if(type(matches) is not str and type(strain1) is not str and type(strain2) is not str and type(combos1) is not str and type(combos2) is not str):
-		prob = 1/(combos1 * combos2)
-		answer1 =  (matches * strain2 * strain1 * prob)
+		prob = np.longdouble(1/(combos1 * combos2))
+		answer1 = np.longdouble(prob * matches * strain2 * strain1)
+		# loganswer = (logm + logs1 + logs2 + logc1 + logc2)
 	else: 
 		answer1 = 'undefined'
-	if(type(overlapping_prob(c, m, n, L)) is not str):
-		answer2 = (((1/3) ** c) * overlapping_prob(c, m, n, L))
-	else:
-		answer2 = 'undefined'
-	# print(str(answer1) + "    " + str(answer2))
-	if(type(answer1) is not str and type(answer2) is not str):
-		if(abs(answer1 - answer2) > .000001):
-			return '			YOU DONE GOOFED'
+		# loganswer = 'undefined'
+	# if(type(overlapping_prob(c, m, n, L)) is not str):
+	# 	answer2 = (((1/3) ** c) * overlapping_prob(c, m, n, L))
+	# else:
+	# 	answer2 = 'undefined'
+	# # print(str(answer1) + "    " + str(answer2))
+	# if(type(answer1) is not str and type(answer2) is not str):
+	# 	if(abs(answer1 - answer2) > .000001):
+	# 		return '			YOU DONE GOOFED'
 	# 	return answer1 - answer2
 	# else:
 	# 	return 'undefined'
 	return answer1
+	# return loganswer
 
 def expected_overlaps(m, n, L):
-	expected = 0
+	expected = np.longdouble(0)
 	x = min(m,n)
 	for o in range(0,(x+1)):
-		expected += (o * overlapping_prob(o, m, n, L))
+		# print(overlapping_prob(o, m, n, L))
+		expected += np.longdouble(o * overlapping_prob(o, m, n, L))
+		# expected += (o * (10 ** overlapping_prob(o, m, n, L)))
+		# print(expected)
+		#log = math.log10(expected)
 	return expected
 
 def expected_matches(m, n, L):
-	expected = 0
+	expected = np.longdouble(0)
 	y = min(m,n)
 	for c in range(0,(y+1)):
-		expected += (c * matching_prob(c, m, n, L, 3))
+		# print(matching_prob(c, m, n, L, 3))
+		expected += np.longdouble(c * matching_prob(c, m, n, L, 3))
 	return expected
--- prob_check_sim.py
+++ prob_check_sim.py
@@ -1,6 +1,7 @@
 from functions import expected_overlaps
 from functions import expected_matches
 from mutation_sim import sim_mutations
+import numpy as np
 
 mu = 0.05
 x = 3
@@ -11,18 +12,18 @@ differences = {}
 
 for z in range(10):
 # for n in range(2):
-	for L in range(5000, 5001):
+	for L in range(3000, 3001):
 		# while 0.01 <= mu <= 0.05:
-		expected['overlaps'] = expected_overlaps(int(mu*L), int(mu*L), L)
-		expected['matches'] = expected_matches(int(mu*L), int(mu*L), L)
+		expected['overlaps'] = np.longdouble(expected_overlaps(int(mu*L), int(mu*L), L))
+		expected['matches'] = np.longdouble(expected_matches(int(mu*L), int(mu*L), L))
 		actual = sim_mutations(n, L, mu)
-		# print('expected: ')
-		# print(expected)
-		# print('actual: ')
-		# print(actual)
+		print('expected: ')
+		print(expected)
+		print('actual: ')
+		print(actual)
 		differences['overlaps'] = expected['overlaps'] - actual['overlaps']
 		differences['matches'] = expected['matches'] - actual['matches']
-		print('the error for ' + str(n) + ' strains of length ' + str(L) + ' with mu = ' + str(mu))
-		print(differences)
+		# print('the error for ' + str(n) + ' strains of length ' + str(L) + ' with mu = ' + str(mu))
+		# print(differences)
 		# mu += 0.01
 # mu = 0.01
