ed5f93df439766290c72ed898161a199d7a18182
schatzk <36019128+schatzkara@users.noreply.github.com>
2018-06-19 23:21:16 -0400

    sim gets id percents now

--- SNPs_sim.py
+++ SNPs_sim.py
@@ -4,10 +4,10 @@
 
 import random
 import numpy as np
-import csv
+from decimal import Decimal
 
 # simulation to mutate 2 DNA strands a given number of times and count up the total number of convergent mutations between them
-# starts with identical but random strands; factors in the probabilites of A,T,C,G; forces a single mutation with each 'generation,'' does not allow for multiple mutations at one site
+# starts with identical but random strands; factors in the probabilites of A,T,C,G; forces a single mutation with each 'generation,' allows for multiple mutations at one site
 # this simulation adds a certain number of SNPs to each DNA strain and counts up the number of convergent mutations that arise after each SNP is added
 # params: 
 # 	L (int) = length of the DNA strands
@@ -32,6 +32,7 @@ def sim_snps(L, generations, GC_prop, kappa, phi):
 
 	# creates the initial strains
 	# time comlexity: O(n), where n is L
+	# AT = 100
 	AT = int(L*(1-GC_prop)) # the number of As and Ts that will be present in the ancestor
 	GC = int(L*(GC_prop)) # the number of Gs and Cs that will be present in the ancestor
 
@@ -46,32 +47,95 @@ def sim_snps(L, generations, GC_prop, kappa, phi):
 		strains[z] = ancestor
 
 	# adds the SNPs to each strain
-	mutation_sites = [generations*[None],generations*[None]] # a list of lists where each list contains the sites that were mutated in the corresponding strand; the strand number is the first index and the generation number in the second index
+	mutation_sites = [[],[]] # a list of lists where each list contains the sites that were mutated in the corresponding strand; the strand number is the first index and the generation number in the second index
 	c = 0 # counter for the number of convergent mutations that have arisen
 	for gen in range(generations): # adds one SNP for each generation
 		for child in range(2): # adds one SNP to each child strand
 			t = list(strains[child])
 			site = random.randint(0,L-1) # the site that is to be mutated
-			while(site in mutation_sites[child]): # forces a new mutation site
-				site = random.randint(0,L-1)
-			mutation_sites[child][gen] = site
-			current = t[mutation_sites[child][gen]]
+			# while(site in mutation_sites[child]): # forces a new mutation site
+			# 	site = random.randint(0,L-1)
+			if(site not in mutation_sites[child]):
+				mutation_sites[child].append(site)
+			current = t[site]
 			# mutates the nucleotide based on the appropriate probabilties
 			if current == 'A':
-				t[mutation_sites[child][gen]] = (random.choices(nucleotides, weights=weights_A, k=1))[0]
+				t[site] = (random.choices(nucleotides, weights=weights_A, k=1))[0]
 			elif current == 'T':
-				t[mutation_sites[child][gen]] = (random.choices(nucleotides, weights=weights_T, k=1))[0]
+				t[site] = (random.choices(nucleotides, weights=weights_T, k=1))[0]
 			elif current == 'G':
-				t[mutation_sites[child][gen]] = (random.choices(nucleotides, weights=weights_G, k=1))[0]
+				t[site] = (random.choices(nucleotides, weights=weights_G, k=1))[0]
 			elif current == 'C':
-				t[mutation_sites[child][gen]] = (random.choices(nucleotides, weights=weights_C, k=1))[0]
+				t[site] = (random.choices(nucleotides, weights=weights_C, k=1))[0]
 			t = ''.join(t)
 			strains[child] = t # replaces the old child with the new one
 		for site in mutation_sites[0]: # counts up the number of convergent mutations
-			if site != None:
-				if(list(strains[0])[site] == list(strains[1])[site]):
-					c += 1
+			if(strains[0][site] == strains[1][site]):
+				c += 1
 		cms[gen] = c
 		c = 0
-		
+
 	return cms
+
+def sim_id_percent(L, generations, GC_prop, kappa, phi):
+	alpha = ((1/L) * kappa)/(kappa + 1) # probability of transitions
+	beta = (1/L)/(kappa + 1) # probability of transversions
+	ancestor = L*[None] # ancestor DNA strand
+	strains = 2*[None] # list of the original DNA sequences
+	# cms = generations*[None] # list of the number of convergent mutations that have arisen up to this point with the index = (number of SNPs - 1) aka (number of generations - 1)
+	id_cms = {}
+	nucleotides = ['A', 'T', 'G', 'C']
+	# the following are the probabilites of mutating to each of the nucleotides depending on the original nucleotide
+	weights_A = [0, (beta*phi), alpha, (beta*(1-phi))]
+	weights_T = [(beta*phi), 0, (beta*(1-phi)), alpha]
+	weights_G = [alpha, (beta*(1-phi)), 0, (beta*phi)]
+	weights_C = [(beta*(1-phi)), alpha, (beta*phi), 0]
+
+	# creates the initial strains
+	# time comlexity: O(n), where n is L
+	# AT = 100
+	AT = int(L*(1-GC_prop)) # the number of As and Ts that will be present in the ancestor
+	GC = int(L*(GC_prop)) # the number of Gs and Cs that will be present in the ancestor
+
+	# generates the ancestral strain according to the number of As, Ts, Gs, and Cs necessary
+	for x in range(AT):
+		ancestor[x] = random.choice(['A', 'T'])
+	for y in range(GC): 
+		ancestor[AT + y] = random.choice(['G', 'C'])
+	random.shuffle(ancestor) # randomly organizes the nucleotides to produce a random ancestor with the correct GC%
+	ancestor = ''.join(ancestor)
+	for z in range(2): # duplicates the ancestor to generate 2 strains
+		strains[z] = ancestor
+
+	# adds the SNPs to each strain
+	mutation_sites = [[],[]] # a list of lists where each list contains the sites that were mutated in the corresponding strand; the strand number is the first index and the generation number in the second index
+	c = 0 # counter for the number of convergent mutations that have arisen
+	for gen in range(generations): # adds one SNP for each generation
+		for child in range(2): # adds one SNP to each child strand
+			t = list(strains[child])
+			site = random.randint(0,L-1) # the site that is to be mutated
+			# while(site in mutation_sites[child]): # forces a new mutation site
+			# 	site = random.randint(0,L-1)
+			if(site not in mutation_sites[child]):
+				mutation_sites[child].append(site)
+			current = t[site]
+			# mutates the nucleotide based on the appropriate probabilties
+			if current == 'A':
+				t[site] = (random.choices(nucleotides, weights=weights_A, k=1))[0]
+			elif current == 'T':
+				t[site] = (random.choices(nucleotides, weights=weights_T, k=1))[0]
+			elif current == 'G':
+				t[site] = (random.choices(nucleotides, weights=weights_G, k=1))[0]
+			elif current == 'C':
+				t[site] = (random.choices(nucleotides, weights=weights_C, k=1))[0]
+			t = ''.join(t)
+			strains[child] = t # replaces the old child with the new one
+		for site in mutation_sites[0]: # counts up the number of convergent mutations
+			if(strains[0][site] == strains[1][site]):
+				c += 1
+		identity = (L - len(mutation_sites[0]) - len(mutation_sites[1]) + c)/L
+		id_cms[gen] = [identity, c]
+		c = 0
+	# print(len(id_cms.keys()))
+	# print(len(id_cms.keys()))
+	return id_cms
--- run_id_sim.py
+++ run_id_sim.py
@@ -3,7 +3,7 @@
 # script to run the SNP simulation according to the users inputs
 # time complexity for each L,GC,kappa combination: O(n^3)
 
-from SNPs_sim import sim_snps
+from SNPs_sim import sim_id_percent
 from decimal import Decimal
 import csv
 
@@ -17,10 +17,10 @@ import csv
 # return: a .csv file will be produced in the directory where this file is located with the data for a single interation (one .csv file for each iteration)
 
 # enter parameters here
-L = [5000]
-generations = [1500]
-GC = [Decimal(0.5)]
-kappa = [1.0]
+L = [1000]
+generations = [300]
+GC = [1.0]
+kappa = [1.0, 2.0, 3.0]
 phi = 1/2
 iterations = 1000
 
@@ -29,9 +29,14 @@ for l in L: # iterates over every length desired
 		for gc in GC: # iterates over every GC% desired
 			for k in kappa: # iterates over every kappa desired
 				for z in range(iterations): # runs it for the desired iterations
-					with open(('SNPs_sim_data_' + str(l) + '_' + str(gc) + '_' + str(k) + '_' + '{0:04}'.format(z+1) + '.csv'), 'w', newline = '') as f: # writes the data to a .csv file
+					with open(('ID_sim_data_' + str(l) + '_' + str(gc) + '_' + str(k) + '_' + '{0:04}'.format(z+1) + '.csv'), 'w', newline = '') as f: # writes the data to a .csv file
 					    writer = csv.writer(f)
-					    writer.writerow(['Mutations on Each Strain', 'CMs']) # column headers
-					    data = [list(range(1,g+1)), (sim_snps(l,g,gc,k,phi))] # one variable is the number of SNPs and the other is the number of convergent mutations
-					    data = zip(*data) # formats the data so it can be written row by row and produce two long columns
-					    writer.writerows(data) # writes the data
+					    writer.writerow(['Mutations on Each Strain', 'ID%', 'CMs']) # column headers
+					    sim = sim_id_percent(l,g,gc,k,phi)
+					    for key in sim.keys():
+
+					    # values = zip(*sim.values())
+					    # data = [(sim.keys()), (values)] # one variable is the number of SNPs and the other is the number of convergent mutations
+					    # data = zip(*data) # formats the data so it can be written row by row and produce two long columns
+					    	writer.writerow([key+1, sim[key][0], sim[key][1]]) # writes the data
+					    # writer.writerows(sim.items()) 
