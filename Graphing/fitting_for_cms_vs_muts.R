f655f8e7a895e254d8748226590bdeaed9655ac0
Zane Billings <39701545+wz-billings@users.noreply.github.com>
2018-06-22 15:56:59 -0400

    model fitting
    These scripts import the data from snps sims and ID sims and fits linear and quadratic models to them. The quadratic models fit extremely well.

---
+++ Graphing/fitting_for_cms_vs_muts.R
@@ -0,0 +1,27 @@
+#this script imports the large CSV and makes graphs.
+setwd("C://Users//insan_000//Documents//summer 2018//simulation data//bigg data")
+library(tidyverse)
+
+dfr = read_csv("all_averages_for_SNP_sim_1000.csv")
+dfr$Kappa = as.factor(dfr$Kappa)
+dfr$`GC%` = as.factor(dfr$`GC%`)
+
+dfrset0 = subset(dfr, dfr$`GC%` == 0)
+
+dfrset0k1 = subset(dfrset0,dfrset0$`Kappa` == 1)
+x <- dfrset0k1$`Mutations on Each Strain`
+y <- dfrset0k1$`Average CMs`
+x2 <- x^2
+
+exponential.model <- lm(log(dfrset0k1$`Mutations on Each Strain`)~dfrset0k1$`Average CMs`)
+linear.model <- lm(dfrset0k1$`Average CMs`~dfrset0k1$`Mutations on Each Strain`)
+quadratic.model <- lm(y~(x2+x))
+
+summary(exponential.model)
+summary(linear.model)
+summary(quadratic.model)
+
+ggplot(dfrset0k1,aes(x=x,y=y)) + geom_point(aes(color="data")) + stat_smooth(method="lm",formula= y~x+I(x^2),linetype=2,aes(color="quadratic model")) + stat_smooth(method="lm",linetype=2,se=F,aes(color="Linear model")) + theme_classic() + labs(title="Modeling convergent mutations vs. total mutations",x="Total mutations",y="Convergent mutations")
+
+ggsave("models_cms_v_muts.png",device="png")
+                                                                                                                                    
