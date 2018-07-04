0f653c68d1824e287100df33861de5da0e3eb962
schatzk <36019128+schatzkara@users.noreply.github.com>
2018-07-04 00:42:04 -0400

    concatenate files

--- Model_and_Simulations/NEW/concatenate_csvs.py
+++ Model_and_Simulations/NEW/concatenate_csvs.py
@@ -4,7 +4,8 @@ import glob
 import os
 
 def concatenate(output_file, data_path):
-	with open((output_file + '.csv'), 'w', newline = '') as f:
+	file_name = os.path.join(data_path, output_file + '.csv')
+	with open((file_name), 'w', newline = '') as f:
 		writer = csv.writer(f)
 		j = 1
 		for filename in glob.glob(os.path.join(data_path, '*.csv')):
