50370b587ac2805a773ec2f820620b27ea04dcbb
Zane Billings <39701545+wz-billings@users.noreply.github.com>
2018-07-05 19:47:35 -0400

    now with more jerry
    yo , i heard you wanted more jerry in your graph so i added more jerry to your graph

--- Graphing/kara_didnt_write_this_script.R
+++ Graphing/kara_didnt_write_this_script.R
@@ -1,11 +1,10 @@
-setwd("C://Users//wzbillin//Desktop")
-
+setwd("C://Users//wzbillin//Documents//monte carlo garph")
 library(gdata)
 library(grid)
 library(tidyverse)
 
 #set up dataframe
-mutdfr <- read_csv("all_mutation_sim_data.csv")
+mutdfr <- read_csv("all_mutation_sim_data_1000.csv")
 # mutdfr$mu <- as.factor(mutdfr$mu)
 # mutdfr$kappa <- as.factor(mutdfr$kappa)
 # mutdfr$phi <- as.factor(mutdfr$phi)
@@ -54,41 +53,14 @@ for (k in loopindex) {
     geom_point(mapping = aes(x = x, y = jerry)) +
     geom_hline(yintercept = ev, linetype = 2, color = "red", size = 1.25) +
     labs(x = "Number of Simulations", 
-    y = "Average convergent mutations", 
-    title = paste("MC_Sim_Graph_mu_",mus[[k]],"_kappa_",kappas[[k]],"_phi_",phis[[k]],"_L_1000.png",sep=""), 
-    subtitle = paste("E[c] = ",ev,sep="")) +
+         y = "Average convergent mutations", 
+         title = paste("MC_Sim_Graph_mu_",mus[[k]],"_kappa_",kappas[[k]],"_phi_",phis[[k]],"_L_1000.png",sep=""), 
+         subtitle = paste("E[c] = ",ev,sep="")) +
     theme_classic()
-    #annotate("text",x = 750, y = (ev*IQR(y)), label = paste("E[c] = ",ev,sep=""), size = 4) 
-    
+  #annotate("text",x = 750, y = (ev*IQR(y)), label = paste("E[c] = ",ev,sep=""), size = 4) 
+  
   print(plot)
   fn = paste("MC_Sim_Graph_mu_",mus[[k]],"_kappa_",kappas[[k]],"_phi_",phis[[k]],"_L_1000.png",sep="")
-  ggsave(fn,device="png")
-    
+  #ggsave(fn,device="png")
+  
 }
-
-# #for loop will create a graph of each mu phi and kappa pair
-# for (k in tab) {
-#   if (i <= 1000) {
-#     #make a graph for the current mu phi kappa
-#     y[[i]] <- k[[2]]
-#     ev <- k[[7]]
-#     i = i+1
-#   } else {
-#     #go to the next mu phi kappa
-#     i = 1
-#     plot <- ggplot() +
-#       geom_point(mapping = aes(x = x, y = y)) +
-#       geom_hline(yintercept = ev, linetype = 2, color = "red", size = 1.25) +
-#       labs(x = "Number of Simulations", 
-#            y = "Average convergent mutations", 
-#            title = paste("MC_Sim_Graph_mu_",k[[4]],"_kappa_",k[[5]],"_phi_",k[[6]],"_L_1000.png",sep=""), 
-#            subtitle = paste("E[c] = ",ev,sep="")) +
-#       theme_classic()
-#       #annotate("text",x = 750, y = (ev*IQR(y)), label = paste("E[c] = ",ev,sep=""), size = 4) 
-#     
-#     print(plot)
-#     fn = paste("MC_Sim_Graph_mu_",k[[4]],"_kappa_",k[[5]],"_phi_",k[[6]],"_L_1000.png",sep="")
-#     ggsave(fn,device="png")
-#       
-#   }
-# }
