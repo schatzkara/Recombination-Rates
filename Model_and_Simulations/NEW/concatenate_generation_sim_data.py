02841b18c22812cf6365e08d9320529f58dbf7c2
schatzkara <36019128+schatzkara@users.noreply.github.com>
2018-07-12 21:59:12 -0400

    Delete concatenate_generation_sim_data.py

--- Model_and_Simulations/NEW/concatenate_generation_sim_data.py
+++
@@ -1,8 +0,0 @@
-
-
-from concatenate_csvs import concatenate
-
-output_file = 'all_generation_sim_data_1000_allGCs'
-data_path = 'D:/BIGG DATA/Generation Sim/all GCs with kappa 0.5,1-6/Generation Sim'
-
-concatenate(output_file, data_path)
