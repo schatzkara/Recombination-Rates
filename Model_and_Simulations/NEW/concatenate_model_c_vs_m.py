86c6e2a6eebc6fcb75e8ea300b6de3558f5604db
schatzkara <36019128+schatzkara@users.noreply.github.com>
2018-07-12 21:59:34 -0400

    Delete concatenate_model_c_vs_m.py

--- Model_and_Simulations/NEW/concatenate_model_c_vs_m.py
+++
@@ -1,7 +0,0 @@
-
-from concatenate_csvs import concatenate
-
-output_file = 'all_model_c_vs_m_data'
-data_path = 'C:/Users/Owner/Documents/UNCG REU/Project/Recombination-Rates/Model_and_Simulations/NEW/Model c vs m'
-
-concatenate(output_file, data_path)
